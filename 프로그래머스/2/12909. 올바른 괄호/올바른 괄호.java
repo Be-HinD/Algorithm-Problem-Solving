4d7c1a61612c86c36d9ccdbc277aed385eeea1a2
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-10-16 11:26:01 +0900

    [level 2] Title: 올바른 괄호, Time: 7.84 ms, Memory: 68.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,20 @@
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        int openCnt = 0;
+        int openCnt = 0; //여는괄호의 중첩카운트
         for(int i=0; i<s.length(); i++) {
             char idx = s.charAt(i);
             if(idx == '(') {//여는 괄호일 시
                 openCnt++;
             } else { //닫는 괄호일 시
-                if(openCnt == 0) {
+                if(openCnt == 0) { //여는괄호의 중첩이 0일 때
                     answer = false;
                     break;
                 }
                 openCnt--;
             }
         }
-        if(openCnt != 0) answer = false;
+        if(openCnt != 0) answer = false; //여는괄호가 다 닫히지 않았을 때
 
         return answer;
     }
