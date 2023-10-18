7ca280484d5bbe798f3eaedc94436c1c1726441d
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-10-18 16:01:57 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 0.28 ms
+메모리: 70.2 MB, 시간: 0.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 15:44:37
+2023년 10월 3일 16:1:54
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,8 +3,8 @@ class Solution {
     static boolean[] v;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        v = new boolean[n];
-        for(int i=0; i<n; i++) {
+        v = new boolean[n]; //컴퓨터 개수 길이 초기화
+        for(int i=0; i<n; i++) { //각 컴퓨터 별로 탐색
             if(v[i]) continue;
             answer += bfs(i, n, computers);
         }
@@ -16,10 +16,10 @@ class Solution {
         q.offer(x);
         while(!q.isEmpty()) {
             int idx = q.poll();
-            v[idx] = true;
+            v[idx] = true; //방문체크
             for(int i=0; i<n; i++) {
-                if(idx == i) continue;
-                if(c[idx][i] == 1) {
+                if(idx == i) continue; //전처리
+                if(c[idx][i] == 1) { //연결되있을 경우
                     //방문체크
                     c[idx][i] = 2;
                     c[i][idx] = 2;
