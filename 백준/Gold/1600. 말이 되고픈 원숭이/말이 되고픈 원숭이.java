9821eb717fe17088d3c5d73840a2242c4b158c55
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-10-21 21:37:27 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 432 ms, Memory: 59120 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -5,9 +5,9 @@ import java.util.*;
 
 public class Main {
     static int K, N, M, res;
-    static int[] dx = new int[]{1,-1,0,0};
+    static int[] dx = new int[]{1,-1,0,0}; //원숭이 방향벡터
     static int[] dy = new int[]{0,0,1,-1};
-    static int[] ndx = new int[]{-2,-2,-1,-1,1,1,2,2};
+    static int[] ndx = new int[]{-2,-2,-1,-1,1,1,2,2}; //말 방향벡터
     static int[] ndy = new int[]{-1,1,-2,2,-2,2,-1,1};
     static int[][] map;
     static boolean[][][] v;
@@ -28,39 +28,33 @@ public class Main {
             }
         }
 
-        if(N == 1 && M == 1) {
+        if(N == 1 && M == 1) { //시작지점 == 도착지점
             System.out.println(0);
             return;
         }
-        v = new boolean[K+1][N][M];
 
-        bfs();
+        v = new boolean[K+1][N][M]; //3차원 방문배열 (나이트 행동횟수만큼)
 
-        if(res == 0) System.out.println(-1);
-        else System.out.println(res);
+        System.out.println(bfs());
     }
 
-    private static void bfs() {
+    private static int bfs() {
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{0,0,0,K}); //좌표, 소요시간, 말 행동횟수
         v[K][0][0] = true;
-        boolean flag;
 
         while(!q.isEmpty()) {
             int[] idx = q.poll();
 
             if(idx[0] == N-1 && idx[1] == M-1) { //도착
-                res = idx[2];
-                return;
+                return idx[2];
             }
 
-            if(idx[3] > 0) flag = true;
-            else flag = false;
-
-            if(flag) { //말처럼 행동할 수 있을 경우
-                for(int i=0; i<8; i++) { //말처럼 처럼 행동할 수도 있고
+            if(idx[3]>0) { //말처럼 행동할 수 있을 경우 : 말의 행동을 할 수도, 원숭이의 행동을 할 수도 2가지의 경우를 전부 체크
+                for(int i=0; i<8; i++) {
                     int nx = idx[0] + ndx[i];
                     int ny = idx[1] + ndy[i];
+                    //현재 기준 방문배열을 확인하는게 아닌 이동할 경우 idx[3]-1의 방문배열을 기준으로 체크해야함!
                     if (nx < 0 || ny < 0 || nx >= N || ny >= M || v[idx[3]-1][nx][ny] || map[nx][ny] == 1) continue;
                     v[idx[3]-1][nx][ny] = true;
                     q.offer(new int[]{nx,ny,idx[2]+1,idx[3]-1});
@@ -75,5 +69,6 @@ public class Main {
                 q.offer(new int[]{nx,ny,idx[2]+1,idx[3]});
             }
         }
+        return -1; //도착하지 못하는 경우
     }
 }
