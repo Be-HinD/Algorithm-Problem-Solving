f19c460f060f0effe180143acb9b54350d8dabcf
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-10-22 00:29:22 +0900

    [Gold III] Title: 파티, Time: 1796 ms, Memory: 69704 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class Main {
     static int N, M, X, res;
+    static int[] Xtoi; //파티장소로부터 각각의 마을까지의 최소비용 배열
     static int[][] map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,17 +24,40 @@ public class Main {
             map[x][y] = weight; //단방향 그래프
         }
 
+        Xtoi = new int[N+1];
+        Arrays.fill(Xtoi, Integer.MAX_VALUE);
+        Xbfs();
         for(int i=1; i<=N; i++) {
             int sum = 0;
+            if(i == X) continue;
             //i번 학생이 i번째 마을에서 출발해서 x마을까지 도착하는데 걸리는 최소비용 구하기
-            sum = bfs(i, X);
-            //i번 학생이 x마을에서 출발해서 i번째 마을까지 도착하는데 걸리는 최소비용 구하기
-            sum += bfs(X, i);
-            //
+            sum = bfs(i, X) + Xtoi[i];
             res = Math.max(res, sum);
         }
 
         System.out.println(res);
+    }
+
+    private static void Xbfs() {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        pq.offer(new int[]{X, 0}); //출발지, 소요시간
+
+        while(!pq.isEmpty()) {
+            int[] idx = pq.poll(); //최소 소요시간부터 꺼내서
+            int cur = idx[0]; //현재 마을
+            for(int i=1; i<N+1; i++) {
+                if(map[cur][i] + idx[1] > Xtoi[i]) continue;
+                if(cur == i || map[cur][i] == 0) continue; //같은 정점이거나 길이 없는 경우
+                Xtoi[i] = idx[1] + map[cur][i]; //최소비용 갱신
+                pq.offer(new int[]{i,idx[1]+map[cur][i]}); //다음 경로 추가
+            }
+        }
     }
 
     private static int bfs(int start, int dest) {
@@ -44,8 +68,8 @@ public class Main {
             }
         });
         int[] dist = new int[N+1]; //start에서 N번째 마을로 가는 최소비용
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        
+        Arrays.fill(dist, Integer.MAX_VALUE); //MAX값 초기화
+
         pq.offer(new int[]{start, 0}); //출발지, 소요시간
 
         while(!pq.isEmpty()) {
@@ -58,12 +82,11 @@ public class Main {
 
             for(int i=0; i<N+1; i++) {
                 if(map[cur][i] > dist[i]) continue;
-                if(cur == i) continue; //같은 정점이거나 방문했던 도로일 경우
-                if(map[cur][i] == 0) continue;
+                if(cur == i || map[cur][i] == 0) continue; //같은 정점이거나 길이 없는 경우
                 dist[i] = map[cur][i];
                 pq.offer(new int[]{i,idx[1]+map[cur][i]});
             }
         }
-        return -1;
+        return 0;
     }
 }
