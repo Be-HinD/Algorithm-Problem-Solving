6e82fe3665182b5daab382d422888190b3427172
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-10-21 20:53:00 +0900

    [level 2] Title: 피로도, Time: 0.22 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -7,18 +7,16 @@ class Solution {
     public int solution(int k, int[][] dungeons) {
         map = dungeons; //입력배열을 전역멤버변수로 변환
         initPirodo = k; //입력 피로도 전역멤버변수로 변환
-        Arrays.sort(map, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0]; //최소 필요 피로도 기준 오름차순 정렬
-            }
-        });
         
-        if(k < map[0][0]) return 0; //아무 던전도 들어갈 수 없는 경우
-        
+        boolean flag = false;
         for(int i=0; i<map.length; i++) {
-            System.out.println(Arrays.toString(map[i]));
+            if(k >= map[i][0]) { //들어갈 수 있는 던전이 있다면
+                flag = true;
+                break;
+            }
         }
+        if(!flag) return 0; //아무 던전도 들어갈 수 없는 경우
+        
         for(int i=1; i<=map.length; i++) { //배열 길이만큼 순열
             arr = new int[i]; //순열 배열 길이 초기화
             v = new boolean[i]; //방문배열 초기화
@@ -27,10 +25,10 @@ class Solution {
         return res;
     }
     
-    private static void Perm(int cnt) {
+    private static void Perm(int cnt) { //순열
         if(cnt == arr.length) {
             //조합 완성
-            if(res == arr.length) return;
+            if(res == arr.length) return; //해당 순열 길이에서 최대값을 이미 찾은 경우
             logic();
         } else {
             for(int i=0; i<arr.length; i++) {
@@ -54,6 +52,6 @@ class Solution {
                 cnt++; //입장 던전 회수 증가
             }
         }
-        res = Math.max(res, cnt);
+        res = Math.max(res, cnt); //결과값 갱신
     }
 }
