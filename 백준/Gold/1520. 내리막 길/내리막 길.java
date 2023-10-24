bb7b8d193fe1e2eb573c48b54faee727e04a09f6
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-10-24 17:08:15 +0900

    [Gold III] Title: 내리막 길, Time: 336 ms, Memory: 39076 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -24,7 +24,7 @@ public class Main {
         }
 
         dp = new int[N][M];
-        for(int i=0; i<N; i++) {
+        for(int i=0; i<N; i++) { //dp배열 -1로 초기화 : 방문체크 용도
             Arrays.fill(dp[i], -1);
         }
 
@@ -33,22 +33,22 @@ public class Main {
     }
 
     private static int dfs(int x, int y) {
-        if(x == N-1 && y == M-1) {
+        if(x == N-1 && y == M-1) { //처음으로 도착할 경우
             return 1;
         }
 
-        if (dp[x][y] != -1) {
+        if (dp[x][y] != -1) { //-1이 아니라면 이미 방문했던 지점
             return dp[x][y];
         }
 
         //동작부
-        dp[x][y] = 0;
+        dp[x][y] = 0; //방문체크
         for(int i=0; i<4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if(nx<0 || ny<0 || nx>=N || ny>=M || map[nx][ny] >= map[x][y]) continue;
-            dp[x][y] += dfs(nx, ny);
+            dp[x][y] += dfs(nx, ny); //현재지점 기준으로 4방향의 길을 전부 탐색 후 각각의 경우의 수를 추가.
         }
-        return dp[x][y];
+        return dp[x][y]; //4방탐색이 종료되면 현재 x,y에서 갈 수 있는 경우의 수를 return.
     }
 }
