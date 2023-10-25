2368277d9ed6822fc2f5b3d3b4c256d1e02ceba7
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-10-25 16:04:17 +0900

    [Gold II] Title: 2048 (Easy), Time: 224 ms, Memory: 18888 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -5,8 +5,6 @@ import java.util.*;
 //2048(Easy) DFS 및 시뮬레이션 풀이
 public class Main {
     static int N, res;
-    static int[] dx = new int[]{-1,1,0,0}; //북,남,동,서
-    static int[] dy = new int[]{0,0,1,-1};
     static int[][] inMap;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -47,19 +45,8 @@ public class Main {
             for(int j=0; j<N; j++) { //깊은 복사
                 copy[j] = map[j].clone();
             }
-            //해당 방향으로 맵 변경
-//            System.out.println("************변경 전*****************");
-//            System.out.println("방향 값 : " + i);
-//            for(int j=0; j<N; j++) {
-//                System.out.println(Arrays.toString(copy[j]));
-//            }
-            logic(copy, i);
 
-//            System.out.println("************변경 후*****************");
-//            System.out.println("방향 값 : " + i);
-//            for(int j=0; j<N; j++) {
-//                System.out.println(Arrays.toString(copy[j]));
-//            }
+            logic(copy, i);
 
             //다음 이동을 위한 재귀호출
             dfs(cnt+1, copy);
@@ -68,6 +55,13 @@ public class Main {
         }
     }
     private static void logic(int[][] map, int dist) {
+        //각 dist에 대해서
+        //i,j 좌표에 대해서
+        //i,j+1부터 비교 시작
+        //비교하는 값이 0이라면 다음 칸 탐색
+        //0이 아닌 수를 찾고 해당 값이 같은 값이라면 현재칸에 증감 및 0으로 배열 값 갱신
+        //다른 값일 경우 현재 칸이 0이면 현재 칸으로 단순 이동 후 계속 탐색 진행!!
+        //현재 칸에 값이 있다면 이동하지않고 break.
         if(dist == 0) { //위
             for(int i=0; i<N; i++) {
                 for(int j=0; j<N; j++) {
@@ -77,12 +71,12 @@ public class Main {
                             if (map[j][i] == map[k][i]) { //합쳐야 할 때
                                 map[j][i] += map[k][i];
                                 map[k][i] = 0;
-                                break;
-                            } else if (map[j][i] != map[k][i]) { //값이 다르면 올리기
-                                if(map[j][i] == 0) {
+                                break; //탈출
+                            } else if (map[j][i] != map[k][i]) { //값이 다르다면
+                                if(map[j][i] == 0) { //현재 칸이 0이라면 현재칸으로 이동
                                     map[j][i] = map[k][i];
                                     map[k][i] = 0;
-                                } else {
+                                } else { //현재 칸에 값이 있다면 이동 x
                                     break;
                                 }
                             }
