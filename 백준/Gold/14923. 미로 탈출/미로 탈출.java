fe77eeb2838686c58bf7ed0da187051db3c7d82d
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-10-27 16:22:47 +0900

    [Gold IV] Title: 미로 탈출, Time: 680 ms, Memory: 116264 KB -BaekjoonHub

---
+++ 백준/Gold/14923. 미로 탈출/미로 탈출.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M, sx, sy, ex, ey, res;
+    static int[] dx = new int[]{1,-1,0,0};
+    static int[] dy = new int[]{0,0,1,-1};
+    static boolean[][][] v;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        sx = Integer.parseInt(st.nextToken())-1;
+        sy = Integer.parseInt(st.nextToken())-1;
+
+        st = new StringTokenizer(br.readLine());
+        ex = Integer.parseInt(st.nextToken())-1;
+        ey = Integer.parseInt(st.nextToken())-1;
+
+        map = new int[N][M];
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        v = new boolean[N][M][2];
+
+        System.out.println(bfs());
+
+    }
+
+    private static int bfs() {
+        PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+               return o1[2] - o2[2];
+            }
+        });
+        q.offer(new int[]{sx,sy,0,1});
+
+        while(!q.isEmpty()) {
+            int[] idx = q.poll();
+            if(idx[0] == ex && idx[1] == ey) {
+                return idx[2];
+            }
+
+            for(int i=0; i<4; i++) {
+                int nx = idx[0] + dx[i];
+                int ny = idx[1] + dy[i];
+                if(nx<0 || ny<0 || nx>=N || ny>=M || v[nx][ny][idx[3]]) continue;
+                if(map[nx][ny] == 1) { //벽
+                    if(idx[3] == 1) { //지팡이 있을 경우
+                        v[idx[0]][idx[1]][idx[3]] = true;
+                        q.offer(new int[]{nx,ny,idx[2]+1,0});
+                    }
+                } else { //빈 칸
+                    v[nx][ny][idx[3]] = true;
+                    q.offer(new int[]{nx, ny, idx[2] + 1, idx[3]});
+                }
+            }
+        }
+        return -1;
+    }
+}
