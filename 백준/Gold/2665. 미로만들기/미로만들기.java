e50b21da791380428f91e106c65de266985674ed
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-10-28 16:59:32 +0900

    [Gold IV] Title: 미로만들기, Time: 84 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -31,23 +31,22 @@ public class Main {
     }
 
     private static int bfs() {
+        //벽 부순 개수를 기준으로 정렬 재정의
         PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 return o1[2] - o2[2];
             }
         });
-        int[][] dp = new int[N][N];
-        for(int i=0; i<N; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        }
-        dp[0][0] = 0;
+        
         pq.offer(new int[]{0,0,0}); //시작좌표, 벽 부순 개수
         v[0][0] = true;
 
         while(!pq.isEmpty()) {
             int[] idx = pq.poll();
-            if(idx[0] == N-1 && idx[1] == N-1) return idx[2];
+            
+            if(idx[0] == N-1 && idx[1] == N-1) return idx[2]; //도착 경우 벽 부순 개수 return
+            
             for(int i=0; i<4; i++) {
                 int nx = idx[0] + dx[i];
                 int ny = idx[1] + dy[i];
@@ -55,10 +54,8 @@ public class Main {
 
 
                 if(map[nx][ny] == 0) { //벽이라면
-//                    if(dp[nx][ny] <= idx[2]) continue;
                     pq.offer(new int[]{nx,ny,idx[2]+1});
                     v[nx][ny] = true;
-                    dp[nx][ny] = idx[2] + 1;
                     continue;
                 }
                 pq.offer(new int[]{nx,ny,idx[2]});
