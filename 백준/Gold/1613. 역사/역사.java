e433be8cfecf64bae5676deea714ea3d382dc290
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-10-31 12:47:00 +0900

    [Gold III] Title: 역사, Time: 496 ms, Memory: 36984 KB -BaekjoonHub

---
+++ 백준/Gold/1613. 역사/역사.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//BOJ_1647 도시 분할 계획
+public class Main {
+    static int N, M, K, res, maxWeight;
+    static int[][] dist;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer();
+
+        N = Integer.parseInt(st.nextToken()); //정점 개수
+        M = Integer.parseInt(st.nextToken()); //간선 개수
+
+        //플로이드 워셜 초기화 단계
+        dist = new int[N][N];
+        for(int i=0; i<N; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+        for(int i=0; i<N; i++) {
+            dist[i][i] = 0;
+        }
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            dist[x][y] = -1; //정방향
+            dist[y][x] = 1; //역방향
+        }
+
+//        System.out.println("****플로이드 전****");
+//        for(int i=0; i<N; i++) {
+//            System.out.println(Arrays.toString(dist[i]));
+//        }
+
+        Floyd();
+
+//        System.out.println();
+//        System.out.println("****플로이드 후****");
+//
+//        for(int i=0; i<N; i++) {
+//            System.out.println(Arrays.toString(dist[i]));
+//        }
+
+        K = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int pre = Integer.parseInt(st.nextToken()) - 1;
+            int post = Integer.parseInt(st.nextToken()) - 1;
+            if(dist[pre][post] == Integer.MAX_VALUE) sb.append(0).append("\n");
+            else sb.append(dist[pre][post]).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    private static void Floyd() {
+        for(int l=0; l<N; l++) { //지나갈 수 있는 간선 개수
+            for(int i=0; i<N; i++) {  //정점
+                for(int j=0; j<N; j++) { //정점
+                    if(dist[i][l] == 1 && dist[l][j] == 1) {
+                        dist[i][j] = 1;
+                    } else if(dist[i][l] == -1 && dist[l][j] == -1) {
+                        dist[i][j] = -1;
+                    }
+                }
+            }
+        }
+    }
+}
