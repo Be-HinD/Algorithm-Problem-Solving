052b6dde890c0d8f68237f7436cfd996e0bb400c
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-11-01 11:49:01 +0900

    [level 1] Title: 모의고사, Time: 0.67 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -7,57 +7,33 @@ class Solution {
         int[] answer = new int[3];
         
         //1번 수포자
-        int cnt = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
         int pointer = 0;
-        int p = 0;
+        int p = -1;
         while(true) {
-            if(answers[pointer] == supo1[p]) cnt++;
-            pointer++;
-            p = (p + 1) % 5;
+            if(answers[pointer] == supo1[(p+1) % 5]) cnt1++;
+            if(answers[pointer] == supo2[(p+1) % 8]) cnt2++;
+            if(answers[pointer++] == supo3[(p+1) % 10]) cnt3++;
+            p++;
             if(pointer == answers.length) break;
         }
-        answer[0] = cnt;
+
+        int Max = Math.max(Math.max(cnt1, cnt2), cnt3);
         
+        int length = 0;
+        if(Max == cnt1) length++;
+        if(Max == cnt2) length++;
+        if(Max == cnt3) length++;
         
-        //2번 수포자
-        cnt = 0;
-        pointer = 0;
+        int[] res = new int[length];
+        
         p = 0;
-        while(true) {
-            if(answers[pointer] == supo2[p]) cnt++;
-            pointer++;
-            p = (p + 1) % 8;
-            if(pointer == answers.length) break;
-        }
-        answer[1] = cnt;
-        
-        //3번 수포자
-        cnt = 0;
-        pointer = 0;
-        p = 0;
-        while(true) {
-            if(answers[pointer] == supo3[p]) cnt++;
-            pointer++;
-            p = (p + 1) % 10;
-            if(pointer == answers.length) break;
-        }
-        answer[2] = cnt;
-        
-        int Max = 0;
-        for(int i=0; i<3; i++) {
-            Max = Math.max(Max, answer[i]);
-        }
-        
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<3; i++) {
-            if(answer[i] == Max) list.add(i);
-        }
-        
-        int[] res = new int[list.size()];
-        
-        for(int i=0; i<list.size(); i++) {
-            res[i] = list.get(i) + 1;
-        }
+        if(Max == cnt1) res[p++] = 1;
+        if(Max == cnt2) res[p++] = 2;
+        if(Max == cnt3) res[p++] = 3;
+   
         return res;
     }
 }
