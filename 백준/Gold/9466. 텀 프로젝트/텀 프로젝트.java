84152d4a5b50d0d9989cc87b864d80aa82c9aec8
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-11-02 16:36:12 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1024 ms, Memory: 301112 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -30,12 +30,12 @@ public class Main {
                 arr[i] = end;
                 if(i == end) { //솔로팀 완성
                     v[i] = true;
-                    res++;
+                    res++; //완성된 팀 개수 증가
                 }
             }
 
             for(int i=0; i<N; i++) {
-                if(!v[i]) {
+                if(!v[i]) { //팀이 완성되지 않은 인원에 대해서만 탐색
                     dfs(i);
                 }
             }
@@ -46,21 +46,20 @@ public class Main {
     }
 
     private static void dfs(int cur) {
-        //방문체크가 된 인원에 대해
-        if(curv[cur]) {
+        //현재 정점이 방문체크가 되었다는 건 : 지나온 경로 상에 인원으로 Cycle이 형성되었다는 걸 의미.
+        if(curv[cur]) { //1 -> 2 -> 3 -> 2 일 경우 2와 3에 대한 Cycle 확인용
             v[cur] = true;
             res++;
-        } else {
-            //방문체크가 되지 않은 인원에 대해
-            curv[cur] = true;
         }
+
+        curv[cur] = true; //현재 인원에 대한 임시 방문체크 true
 
         int next = arr[cur]; //다음 학생
         if(!v[next]) {
             dfs(next);
         }
 
-        curv[cur] = false;
-        v[cur] = true;
+        curv[cur] = false; //방문체크는 무조건 초기화
+        v[cur] = true; //
     }
 }
