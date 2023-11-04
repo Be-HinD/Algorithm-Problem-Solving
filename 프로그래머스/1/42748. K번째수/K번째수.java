6b146f9874c113fb0732deb01f202baec172ed93
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-11-04 16:59:28 +0900

    [level 1] Title: K번째수, Time: 0.60 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer;
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<commands.length; i++) {
+            int leng = (commands[i][1]-commands[i][0])+1;
+            int[] arr = new int[leng];
+            int p = commands[i][0] - 1;
+            for(int j=0; j<leng; j++) {
+                arr[j] = array[p++];
+            }
+            Arrays.sort(arr);
+            
+            int searchNum = commands[i][2] - 1;
+            list.add(arr[searchNum]);
+        }
+        
+        answer = new int[list.size()];
+        int p = 0;
+        for(int idx : list) {
+            answer[p++] = idx;
+        }
+        return answer;
+    }
+}
