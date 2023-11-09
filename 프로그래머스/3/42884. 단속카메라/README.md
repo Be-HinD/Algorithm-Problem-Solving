19cf93fd3396723db0fac85731517e7a128adb28
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-11-09 13:55:26 +0900

    [level 3] Title: 단속카메라, Time: 7.41 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
 
 ### 성능 요약
 
-메모리: 58.1 MB, 시간: 19.07 ms
+메모리: 53.5 MB, 시간: 7.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 13:51:38
+2023년 11월 4일 13:55:25
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -3,6 +3,7 @@ class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
         
+        //진입,진출 배열의 진출시간을 기준으로 오름차순 정렬
         Arrays.sort(routes, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -10,18 +11,22 @@ class Solution {
             }
         });
         
-        int camera = Integer.MIN_VALUE;
+        //제일 시작 부분에 카메라를 설치
+        int camera = -30000;
+        
         for(int i=0; i<routes.length; i++) {
             if(camera < routes[i][0]) {
+                //현재 설치된 카메라보다 이번 차량의 진입시간이 클 경우
+                //== -15 지점에 1개를 설치하였는데
+                //다음 차량의 진입시간이 -18이고, 오름차순 정렬에 의해
+                //다음 차량의 진출시간이 -15보다 더 크다는걸 알고 있기에
+                //넘어가고
+                //이 후 -14의 진입시간을 가진 차와 비교 시 카메라를 새롭게 설치.
                 camera = routes[i][1];
                 answer++;
             }
         }
         
-        
-        
-        //15 13 5 3
-        //System.out.println(routes[0][1]);
         return answer;
     }
 }
