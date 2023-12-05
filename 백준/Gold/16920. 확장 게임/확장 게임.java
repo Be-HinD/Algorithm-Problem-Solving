064f4582b8b0e154445490a19aa84ce5f17ede10
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-12-05 19:04:27 +0900

    [Gold II] Title: 확장 게임, Time: 612 ms, Memory: 96404 KB -BaekjoonHub

--- 백준/Gold/16920. 확장 게임/확장 게임.java
+++ 백준/Gold/16920. 확장 게임/확장 게임.java
@@ -27,12 +27,14 @@ public class Main {
         for(int i=0; i<=P; i++) casttleList.add(new ArrayList<>());
 
 
+        //이동 가능거리 Si 입력
         dist = new int[P+1];
         st = new StringTokenizer(br.readLine());
         for(int i=1; i<=P; i++) {
             dist[i] = Integer.parseInt(st.nextToken());
         }
 
+        //맵 입력
         map = new char[N][M];
         for(int i=0; i<N; i++) {
             String input = br.readLine();
@@ -40,6 +42,7 @@ public class Main {
                 char idx = input.charAt(j);
                 map[i][j] = idx;
                 if(idx != '.' && idx != '#') {
+                    //입력이 성일 경우 각 플레이어 별 Queue에 추가
                     int player = idx - '0';
                     casttleList.get(player).add(new int[]{i,j, 0});
                     res[player]++;
@@ -48,12 +51,12 @@ public class Main {
         }
 
         while(true) {
-            int cnt = 0;
-
+            int sum = 0;
             for(int i=1; i<=P; i++) {
-                cnt += bfs(i);
+                bfs(i);
+                sum += casttleList.get(i).size();
             }
-            if(cnt == 0) break;
+            if(sum == 0) break; //확장된 칸이 없을 경우
         }
 
         for(int i=1; i<=P; i++) {
@@ -61,33 +64,33 @@ public class Main {
         }
     }
 
-    private static int bfs(int player) {
+    private static void bfs(int player) {
         Queue<int[]> q = new ArrayDeque<>();
-        boolean[][] v = new boolean[N][M];
+//        boolean[][] v = new boolean[N][M];
         for(int[] idx : casttleList.get(player)) {
             q.offer(idx);
-            v[idx[0]][idx[1]] = true;
+//            v[idx[0]][idx[1]] = true;
         }
-        casttleList.get(player).clear();
-        int cnt = 0;
+        casttleList.get(player).clear(); //다음 턴에서 큐에 추가할 좌표를 최소화(하지않을 경우 70% 시간초과 발생)
+
         while(!q.isEmpty()) {
             int[] idx = q.poll();
-            if(idx[2] == dist[player]) continue;
+            if(idx[2] == dist[player]) {
+                casttleList.get(player).add(new int[]{idx[0],idx[1],0});
+                continue;
+            }
 
             for(int i=0; i<4; i++) {
                 int nx = idx[0] + dx[i];
                 int ny = idx[1] + dy[i];
-                if(nx<0 || ny<0 || nx>=N || ny>=M || v[nx][ny]) continue;
+                if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
                 if(map[nx][ny] == '.') { //확장 가능할 경우에만
-                    casttleList.get(player).add(new int[]{nx,ny,0});
-                    v[nx][ny] = true;
-                    map[nx][ny] = (char) player;
+//                    v[nx][ny] = true;
+                    map[nx][ny] = '#';
                     q.offer(new int[]{nx,ny, idx[2]+1});
-                    cnt++;
                     res[player]++;
                 }
             }
         }
-        return cnt;
     }
 }
