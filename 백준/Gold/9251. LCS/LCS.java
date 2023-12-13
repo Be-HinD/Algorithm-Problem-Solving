3884eea13d3b56603f445c9df76d7df0afc9ed15
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2023-12-13 16:59:42 +0900

    [Gold V] Title: LCS, Time: 104 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//BOJ_9251 LCS
+public class Main {
+    static String a, b;
+    static char[] arr1, arr2;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        a = br.readLine();
+        arr1 = new char[a.length()+1];
+        for(int i=0; i<a.length(); i++) {
+            arr1[i+1] = a.charAt(i);
+        }
+        b = br.readLine();
+        arr2 = new char[b.length()+1];
+        for(int i=0; i<b.length(); i++) {
+            arr2[i+1] = b.charAt(i);
+        }
+        int[][] dp = new int[a.length()+1][b.length()+1];
+        for(int i=1; i<=a.length(); i++) {
+            for(int j=1; j<=b.length(); j++) {
+                if(arr1[i] == arr2[j]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        System.out.println(dp[arr1.length-1][arr2.length-1]);
+    }
+}
