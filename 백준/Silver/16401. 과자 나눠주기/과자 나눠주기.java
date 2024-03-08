0ea7d8529cba879eea4a7a026a2fe9910b8f1dea
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2024-03-08 13:17:57 +0900

    [Silver II] Title: 과자 나눠주기, Time: 988 ms, Memory: 171236 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//BOJ_16401 과자 나눠주기
+public class Main {
+    static int N,M;
+    static int[] arr;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<M; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int res = upperBound();
+        System.out.println(res==-1?0:res);
+    }
+
+    public static int upperBound() {
+        int low = 0;
+        int high = 1000000055;
+        while (low < high) {
+            final int mid = (low + high) /2;
+            if(mid == 0) return -1;
+            int cnt = 0;
+
+            for(int i=0; i<M; i++) {
+                if(mid <= arr[i]) {
+                    cnt += arr[i] / mid;
+                }
+            }
+
+            if (cnt >= N) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low-1;
+    }
+
+}
