f94599ef6e4a915e6f452513b22c56474e142411
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2024-03-23 18:32:05 +0900

    [Gold III] Title: 두 배열의 합, Time: 464 ms, Memory: 25400 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -1,90 +1,87 @@
-    import java.io.*;
-    import java.util.*;
+import java.io.*;
+import java.util.*;
 
-    //BOJ_2143 두 배열의 합
-    public class Main {
-        static int N;
-        static long res;
-        static int[] a, b;
-        static long[] na, nb;
-        public static void main(String[] args) throws Exception {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
+//BOJ_2143 두 배열의 합
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());    //타겟 넘버
+        int aN = Integer.parseInt(br.readLine());    //A배열 길이
 
-            HashMap<Long, Integer> A_data = new HashMap<>();
-            HashMap<Long, Integer> B_data = new HashMap<>();
+        int[] a = new int[aN];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<aN; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
 
-            na = new long[500600];
-            nb = new long[500600];
-            N = Integer.parseInt(br.readLine());
+        int bN = Integer.parseInt(br.readLine());
+        int[] b = new int[bN];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<bN; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
 
-            int lengA = Integer.parseInt(br.readLine());
-            a = new int[lengA];
-
-            st = new StringTokenizer(br.readLine());
-            for(int i=0; i<lengA; i++) {
-                a[i] = Integer.parseInt(st.nextToken());
+        //A의 모든 구간합을 배열로 만들고 O(2N)
+        //T에서 B의 구간합 sum을 뺀 x를 이진탐색 upper - lower 로 찾기 O(NlogN)
+        //res += 이진탐색 결과
+        //N = 1000, O(1000log1000)
+        long[] secionSum = new long[500600];
+        Arrays.fill(secionSum, Integer.MAX_VALUE);
+        int p = 0;
+        for(int i=0; i<aN; i++) {
+            long sum = 0;
+            for(int j=i; j<aN; j++) {
+                sum += a[j];
+                secionSum[p++] = sum;
             }
+        }
 
-            int lengB = Integer.parseInt(br.readLine());
-            b = new int[lengB];
+        Arrays.sort(secionSum);
 
-            st = new StringTokenizer(br.readLine());
-            for(int i=0; i<lengB; i++) {
-                b[i] = Integer.parseInt(st.nextToken());
+        long res = 0;
+        for(int i=0; i<bN; i++) {
+            long sum = 0;
+            for(int j=i; j<bN; j++) {
+                sum += b[j];
+                res += upper(secionSum, T-sum,p) - lower(secionSum, T-sum,p);
             }
+        }
+        System.out.println(res);
 
+    }
 
+    private static int upper(long[] arr, long key,int high) {
+        int low = 0;
 
-            //A 누적합을 전부 계산
-            int p = 0;
-            int naCnt = 0;
-            for(int i=0; i<lengA; i++) {
-                for(int j=i; j<lengA; j++) {
-                    if(i==j) {
-                        //초기화
-                        na[p++] = a[i];
-                    }
-                    else {
-                        na[p++] = na[p-2] + a[j];
-                    }
-                    if(A_data.get(na[p-1]) != null) {
-                        int idx = A_data.get(na[p-1]);
-                        A_data.put(na[p-1], ++idx);
-                    }
-                    else A_data.put(na[p-1], 1);
-                    naCnt++;
-                }
+        while(low < high) {
+            final int mid = (low + high) >> 1;
+
+            if(arr[mid] <= key) {
+                low = mid + 1;
             }
-
-            //B 누적합을 전부 계산
-            p = 0;
-            for(int i=0; i<lengB; i++) {
-                for(int j=i; j<lengB; j++) {
-                    if(i==j) {
-                        //초기화
-                        nb[p++] = b[i];
-                    }
-                    else {
-                        nb[p++] = nb[p-2] + b[j];
-                    }
-                    if(B_data.get(nb[p-1]) != null) {
-                        int idx = B_data.get(nb[p-1]);
-                        B_data.put(nb[p-1], ++idx);
-                    }
-                    else B_data.put(nb[p-1], 1);
-                }
+            else {
+                high = mid;
             }
-
-            for(int i=0; i<naCnt; i++) {
-                long idx = na[i];
-                long key = N - idx;
-                if(B_data.get(key) != null) {
-                    res += B_data.get(key);
-                }
-            }
-
-            System.out.println(res);
 
         }
+        return low;
     }
+
+    private static int lower(long[] arr, long key, int high) {
+        int low = 0;
+
+        while(low < high) {
+            final int mid = (low + high) >> 1;
+
+            if(arr[mid] >= key) {
+                high = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+
+        }
+        return low;
+    }
+}
