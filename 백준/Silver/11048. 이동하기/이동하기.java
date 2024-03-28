98188c43e6763fb89548f8a9694e52fe1ec074cd
Kimjeonghyeon <78507813+Be-HinD@users.noreply.github.com>
2024-03-29 00:27:17 +0900

    [Silver II] Title: 이동하기, Time: 408 ms, Memory: 77760 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -10,22 +10,14 @@ public class Main {
         int N = Integer.parseInt(st.nextToken()); //배낭 무게
         int M = Integer.parseInt(st.nextToken()); //귀금속 종류
 
-        int[][] arr = new int[N+1][M+1];
+
+        int[][] dp = new int[N+1][M+1];
         for(int i=1; i<=N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=1; j<=M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        //아래, 오른쪽, 대각선
-        int[][] dp = new int[N+1][M+1];
-
-        //dp[i][j] = i,j위치에서 먹을 수 있는 최대 사탕개수
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=M; j++) {
-                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + arr[i][j];
-                dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1] + arr[i][j]);
+                int idx = Integer.parseInt(st.nextToken());
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + idx;
+                dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1] + idx);
             }
         }
 
