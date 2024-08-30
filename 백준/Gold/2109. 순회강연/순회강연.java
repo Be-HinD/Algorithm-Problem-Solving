169fded86b3998b6f5b00c955537b02959242862
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-08-30 15:56:30 +0900

    [Gold III] Title: 순회강연, Time: 204 ms, Memory: 18480 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -10,39 +10,30 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[0] == o2[0]) {
-                    return o1[1] - o2[1];
-                }
-                return o2[0] - o1[0];   //페이 기준 내림차순
-            }
-        });
-
+        List<int[]> list = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            pq.offer(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+            list.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
         }
 
-        boolean[] check = new boolean[10001];
+        Collections.sort(list, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        for(int i=0; i<list.size(); i++) {
+            pq.offer(list.get(i)[0]);
+            if(pq.size() > list.get(i)[1]) {
+                pq.poll();
+            }
+        }
 
         while(!pq.isEmpty()) {
-            if(!check[pq.peek()[1]]) {
-                check[pq.peek()[1]] = true;
-                res += pq.poll()[0];
-            }
-            else {
-                int[] cur = pq.poll();
-                for(int i=cur[1]; i>0; i--) {
-                    if(!check[i]) {
-                        check[i] = true;
-                        res += cur[0];
-                        break;
-                    }
-                }
-            }
+            res += pq.poll();
         }
 
         System.out.println(res);
