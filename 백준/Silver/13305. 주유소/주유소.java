60072e3121da64d54e98ea0c209e5b15917454ac
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-10-05 21:43:25 +0900

    [Silver III] Title: 주유소, Time: 228 ms, Memory: 31800 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+//BOJ_13305
+public class Main {
+    static int cityCnt;
+    static int[] dist, store;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        cityCnt = Integer.parseInt(br.readLine());
+        dist = new int[cityCnt-1];
+        store = new int[cityCnt];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<dist.length; i++) {
+            dist[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<store.length; i++) {
+            store[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int res = 0;
+
+        for(int i=0; i<store.length-1; i++) {
+            int curCost = store[i];
+            res += dist[i] * curCost;
+
+            //현재 지점보다 저렴한지점까지 탐색
+            for(int j=i+1; j<store.length-1; j++) {
+                if(j == store.length-1) break;
+
+                if(curCost <= store[j]) {
+                    res += dist[j] * curCost;
+                    i++;
+                }
+            }
+        }
+
+        System.out.println(res);
+
+    }
+}
