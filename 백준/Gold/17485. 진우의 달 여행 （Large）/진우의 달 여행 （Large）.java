63c257d510c07da85bbe68c1e88bacf7b2167b93
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-10-24 06:24:06 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 488 ms, Memory: 114252 KB -BaekjoonHub

--- 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
@@ -1,9 +1,11 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+//BOJ_17485
 public class Main {
-    static int N, M;
-    static int[][] arr;
+    static int N, M, res;
+    static int[][] map;
     static int[][][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,49 +14,59 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[N][M];
-        dp = new int[N][M][3];
+        map = new int[N][M];
 
-        for(int i = 0 ; i < N ; i ++){
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0 ; j < M ; j ++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-
-                Arrays.fill(dp[i][j], 1_000_100);
+            for(int j=0; j<M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i = 0 ; i < M ; i ++){
-            for(int j = 0 ; j < 3 ; j ++){
-                dp[0][i][j] = arr[0][i]; // dp 초기값 저장
+        /**
+         * BFS 풀이의 경우 시작지점 1000개에 대해 각각 돌려야하므로 O(1000 * 1000 * 1000) = 10억으로 시간초과
+         * 메모이제이션을 통해 i,j에대해 이전 행동에 대한 최소값을 갱신
+         * 왼쪽위, 위, 오른쪽위
+         * **/
+
+        dp = new int[N][M][3];  //3가지 움직임에 대한 3차원 배열
+
+        for(int i=0; i<M; i++) {    //시작점 초기화
+            dp[0][i][0] = map[0][i];
+            dp[0][i][1] = map[0][i];
+            dp[0][i][2] = map[0][i];
+        }
+
+        for(int i=1; i<N; i++) {
+            for(int j=0; j<M; j++) {
+
+                if (j == 0) {
+                    dp[i][j][0] = 10000000;
+                    dp[i][j][1] = Math.min(dp[i-1][j][0] + map[i][j], dp[i-1][j][2] + map[i][j]);
+                    dp[i][j][2] = Math.min(dp[i-1][j+1][0] + map[i][j], dp[i-1][j+1][1] + map[i][j]);
+                }
+                else if(j == M-1) {
+                    dp[i][j][0] = Math.min(dp[i-1][j-1][1] + map[i][j], dp[i-1][j-1][2] + map[i][j]);
+                    dp[i][j][1] = Math.min(dp[i-1][j][0] + map[i][j], dp[i-1][j][2] + map[i][j]);
+                    dp[i][j][2] = 10000000;
+                }
+                else {
+                    dp[i][j][0] = Math.min(dp[i-1][j-1][1] + map[i][j], dp[i-1][j-1][2] + map[i][j]);
+                    dp[i][j][1] = Math.min(dp[i-1][j][0] + map[i][j], dp[i-1][j][2] + map[i][j]);
+                    dp[i][j][2] = Math.min(dp[i-1][j+1][0] + map[i][j], dp[i-1][j+1][1] + map[i][j]);
+                }
+
             }
         }
 
-        // 왼쪽 0, 가운데 1, 오른쪽 2
-        for(int i = 1 ; i < N ; i ++){ // 행
-            for(int j = 0 ; j < M ; j ++){ // 열
-                if(j == 0){ // 열이 0이라면 => 가운데, 오른쪽 에서 올 수 있음
-                    dp[i][j][1] = dp[i-1][j][2] + arr[i][j];
-                    dp[i][j][2] = Math.min(dp[i-1][j+1][1], dp[i-1][j+1][0]) + arr[i][j];
-                }
-                else if(j < M-1){ // 왼쪽, 가운데, 오른쪽에서 올 수 있음
-                    dp[i][j][2] = Math.min(dp[i - 1][j + 1][1], dp[i - 1][j + 1][0]) + arr[i][j];
-                    dp[i][j][1] = Math.min(dp[i - 1][j][0], dp[i - 1][j][2]) + arr[i][j];
-                    dp[i][j][0] = Math.min(dp[i - 1][j - 1][2], dp[i - 1][j - 1][1]) + arr[i][j];
-                }
-                else{
-                    dp[i][j][0] = Math.min(dp[i - 1][j - 1][1], dp[i - 1][j - 1][2]) + arr[i][j];
-                    dp[i][j][1] = dp[i-1][j][0] + arr[i][j];
-                }
+        res = Integer.MAX_VALUE;
+        for(int i=0; i<M; i++) {
+            for(int j=0; j<3; j++) {
+                res = Math.min(res, dp[N-1][i][j]);
             }
         }
 
-        int ans = 1 << 30;
-        for (int i = 0; i < M; i ++) {
-            for (int j = 0; j < 3; j ++)
-                ans = Math.min(ans, dp[N - 1][i][j]);
-        }
+        System.out.println(res);
 
-        System.out.println(ans);
     }
 }
