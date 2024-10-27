5b44e1752b5adad8723850811da7e1d893274ad1
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-10-27 23:48:03 +0900

    [level 3] Title: 풍선 터트리기, Time: 45.18 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -9,38 +9,26 @@ class Solution {
         left[i] = i에서의 최소값
         right[i] = i에서의 최소값
         **/
-        if(a.length == 1) return 1;
+        
+        if(a.length == 1) return 1; //입력 조건 처리
         
         int[] left = new int[a.length];
         int[] right = new int[a.length];
         
         left[0] = a[0];
         for(int i=1; i<a.length; i++) {
-            if(a[i] < left[i-1]) {
-                left[i] = a[i];
-            }
-            else {
-                left[i] = left[i-1];
-            }
+            if(a[i] < left[i-1]) left[i] = a[i];
+            else left[i] = left[i-1];
         }
         
         right[a.length-1] = a[a.length-1];
         for(int i=a.length-2; i>=0; i--) {
-            if(a[i] < right[i+1]) {
-                right[i] = a[i];
-            }
-            else {
-                right[i] = right[i+1];
-            }
+            if(a[i] < right[i+1]) right[i] = a[i];
+            else right[i] = right[i+1];
         }
         
         for(int i=1; i<a.length-1; i++) {
-            int cnt = 0;
-            
-            if(left[i-1] < a[i]) cnt++;
-            if(right[i+1] < a[i]) cnt++;
-            if(cnt==2) continue;
-            answer++;
+            if(left[i-1] > a[i] || right[i+1] > a[i]) answer++;
         }
         return answer;
     }
