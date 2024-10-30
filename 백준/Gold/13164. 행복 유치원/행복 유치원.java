4958d16bc922bf08c360f9ff3ada4cde92234f19
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-10-30 11:03:38 +0900

    [Gold V] Title: 행복 유치원, Time: 664 ms, Memory: 69348 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -4,17 +4,9 @@
     //BOJ_13164
     public class Main {
         static int N, K, res;
-        static int[] arr;
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             StringTokenizer st = new StringTokenizer(br.readLine());
-
-            /**
-             * 적어도 한 명
-             * 같은 조에 속한 원생들은 서로 인접
-             * 가장 키가 큰 원생과 가장 키가 작은 원생의 키 차이
-             * K개의 조에 대해 티셔츠 만드는 비용의 합을 최소
-             * **/
 
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
@@ -24,16 +16,14 @@
                 return;
             }
 
-            arr = new int[N];
             st = new StringTokenizer(br.readLine());
-            for(int i=0; i<N; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-            }
-
             List<Integer> list = new ArrayList<>();
 
-            for(int i=1; i<N; i++) {
-                list.add(arr[i]-arr[i-1]);
+            int prev = Integer.parseInt(st.nextToken());
+            for(int i=0; i<N-1; i++) {
+                int idx = Integer.parseInt(st.nextToken());
+                list.add(idx-prev);
+                prev = idx;
             }
 
             Collections.sort(list);
