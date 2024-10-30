87841a7b752da368ca67cf02b143c9d4be276597
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-10-30 11:38:24 +0900

    [Gold V] Title: 센서, Time: 148 ms, Memory: 15096 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -1,39 +1,41 @@
-import java.io.*;
-import java.util.*;
+    import java.io.*;
+    import java.util.*;
 
-//BOJ_2212
-public class Main {
-    static int N, K, res;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+    //BOJ_13164
+    public class Main {
+        static int N, K, res;
+        static int[] arr;
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());   //센서 개수
-        K = Integer.parseInt(br.readLine());   //집중국 개수
+            N = Integer.parseInt(br.readLine());
+            K = Integer.parseInt(br.readLine());
 
-        int[] sencor = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            sencor[i] = Integer.parseInt(st.nextToken());
+            if(N == 1) {
+                System.out.println(0);
+                return;
+            }
+
+            arr = new int[N];
+
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for(int i=1; i<N; i++) {
+                pq.offer(arr[i] - arr[i-1]);
+            }
+
+            for(int i=0; i<N-K; i++) {
+                res += pq.poll();
+            }
+
+            System.out.println(res);
+
         }
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(((o1, o2) -> {return o2 - o1;}));
-
-        Arrays.sort(sencor);
-        
-        for(int i=N-1; i>0; i--) {
-            pq.offer(sencor[i] - sencor[i-1]);
-        }
-
-        for(int i=0; i<K-1; i++) {
-            pq.poll();
-        }
-
-        while(!pq.isEmpty()) {
-            res += pq.poll();
-        }
-
-        System.out.println(res);
-
     }
-}
