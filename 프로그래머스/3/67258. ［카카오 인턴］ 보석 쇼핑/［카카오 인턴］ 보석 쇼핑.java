45c53714ee67447848850fbbb4056dff6b785817
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-10-30 16:00:34 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 68.51 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = {};
+        int[] answer = new int[2];
         
         //SET자료구조로 종류의 개수 탐색
         //슬라이딩윈도우로 모든 종류를 구매할 수 있는 길이 탐색
@@ -14,10 +14,9 @@ class Solution {
         int l=0;
         
         Map<String, Integer> map = new HashMap<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> 
-                                                      {if(o1[2] == o2[2]) return o1[0] - o2[0];
-                                                                    return o1[2] - o2[1];});
-        int minSize = gems.length;
+        
+        int minSize = gems.length;  //구간의 최소길이
+        
         for(int i=0; i<gems.length; i++) {
             map.put(gems[i], map.getOrDefault(gems[i],0) + 1);
             if(map.size() != cnt) continue;
@@ -29,12 +28,12 @@ class Solution {
             
             if(i-l < minSize) {
                 minSize = i-l;
-                pq.offer(new int[]{l+1,i+1, i-l});
+                answer[0] = l+1;
+                answer[1] = i+1;
             }
             
         }
         
-        int[] res = pq.poll();
-        return new int[]{res[0], res[1]};
+        return answer;
     }
 }
