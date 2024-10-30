29a50dfa540af15eada70f8f021165b6d22c0319
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-10-30 13:34:29 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 75.40 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 보석 쇼핑 - 67258 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258) 
 
 ### 성능 요약
 
-메모리: 80.8 MB, 시간: 51.53 ms
+메모리: 96.6 MB, 시간: 75.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 05일 20:46:57
+2024년 10월 30일 13:34:27
 
 ### 문제 설명
 
--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,39 +1,40 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] gems) {
+        int[] answer = {};
         
-        //1번에서부터 다 포함하는 지점 탐색
-        //1번부터 하나씩 빼서 조건 만족하는 구간 탐색
-        Set<String> condition = new HashSet<>();
-        //카운팅 배열 쓰고 싶은데
-        for(int i=0; i<gems.length; i++) {
-            if(!condition.contains(gems[i])) {
-                condition.add(gems[i]);
-            }
-        }
+        //SET자료구조로 종류의 개수 탐색
+        //슬라이딩윈도우로 모든 종류를 구매할 수 있는 길이 탐색
         
-        //탐색하면서 set 삭제
+        Set<String> set = new HashSet<>();
+        for(String gem : gems) set.add(gem);
+        
+        int cnt = set.size();
+        
+        int l=0;
+        
         Map<String, Integer> map = new HashMap<>();
-        
-        int l = 0, left = 0, right = 0;
-        int leng = Integer.MAX_VALUE;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> 
+                                                      {if(o1[2] == o2[2]) return o1[0] - o2[0];
+                                                                    return o1[2] - o2[1];});
+        int minSize = gems.length;
         for(int i=0; i<gems.length; i++) {
-            
-            map.put(gems[i], map.getOrDefault(gems[i], 0) + 1);
-            
+            map.put(gems[i], map.getOrDefault(gems[i],0) + 1);
+            if(map.size() != cnt) continue;
+        
             while(map.get(gems[l]) > 1) {
-                map.put(gems[l], map.get(gems[l]) - 1);
+                map.put(gems[l], map.get(gems[l])-1);
                 l++;
             }
             
-            if(map.size() == condition.size() && (i - l) < leng) {
-                leng = i - l;
-                left = l;
-                right = i;
+            if(i-l < minSize) {
+                minSize = i-l;
+                pq.offer(new int[]{l+1,i+1, i-l});
             }
+            
         }
         
-                
-        return new int[]{left+1, right+1};
+        int[] res = pq.poll();
+        return new int[]{res[0], res[1]};
     }
 }
