1dc13ae486fc763df5e84e2642c2ff94b2dd4c53
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-11-15 16:52:02 +0900

    [Gold V] Title: 나의 인생에는 수학과 함께, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Gold/17265. 나의 인생에는 수학과 함께/나의 인생에는 수학과 함께.java
+++ 백준/Gold/17265. 나의 인생에는 수학과 함께/나의 인생에는 수학과 함께.java
@@ -17,8 +17,7 @@ public class Main {
          *  집 (1, 1)에서 학교 (N, N)까지 최단거리로 이동
          *  오른쪽과 아래쪽으로만 이동
          *  접근법
-         *  최단거리 == 다익스트라
-         *  임의의 최단거리 S와 동일한 모든 경로에서 최대값 최소값을 갱신
+         *  임의의 최단거리 S(2N -1)와 동일한 모든 경로에서 최대값 최소값을 갱신
          *  모든 경로 탐색 == DFS
          * **/
 
@@ -44,8 +43,9 @@ public class Main {
     static int[] dx = new int[]{1,0};
     static int[] dy = new int[]{0,1};
     private static void dfs(int x, int y, char prevOper, int sum, int cnt) {
-        if(cnt >= minDist) return;
-        if(map[x][y] == '0' || map[x][y] == '1' || map[x][y] == '2' || map[x][y] == '3' || map[x][y] == '4' || map[x][y] == '5') {
+        if(cnt >= minDist) return;  //최단거리 백트래킹
+        
+        if((x%2==0&&y%2==0) || (x%2!=0&&y%2!=0)) {
             //숫자인 경우
             if(prevOper == '+') {
                 sum += map[x][y] - '0';
