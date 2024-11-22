9e7894bafabdf7ce7c7602718bbbe2c7523930cd
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-11-22 19:00:37 +0900

    [level 3] Title: 110 옮기기, Time: 140.32 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
@@ -1,43 +1,74 @@
 import java.util.*;
-
 class Solution {
     public String[] solution(String[] s) {
-        String[] answer = new String[s.length];
         
-        for(int i=0; i<s.length; i++){
-            String ans = solve(s[i]);   
-            answer[i] = ans;
+        /**
+        x를 최대한 사전 순으로
+        x에 있는 "110"을 뽑아서
+        임의의 위치에 다시 삽입
+        접근법
+        110을 뽑을 수 있는대로 전부 추출
+        남은 x에서 뽑은 110들을 삽입할 위치 탐색
+        마지막 0의 위치 뒤에 삽입
+        **/
+        
+        String[] res = new String[s.length];
+        int p = 0;
+        for(String idx : s) {
+            res[p++] = process(idx);
         }
-        
-        return answer;
+        return res;
     }
     
-    public String solve(String s){
-        StringBuilder sb = new StringBuilder();
-        StringBuilder ooz = new StringBuilder();
-        
-        for(int i=0; i<s.length(); i++){
-            Character c = s.charAt(i);
-            if(sb.length()>=2 && c=='0' && sb.charAt(sb.length()-2)=='1' && sb.charAt(sb.length()-1)=='1'){
-                ooz.append("110");
-                sb.delete(sb.length()-2, sb.length());
-            }
-            else{
-                sb.append(c);
+    private static String process(String idx) {
+        String remainStr = extract110(idx);
+        int cnt110 = (idx.length() - remainStr.length()) / 3;
+        //삽입위치 탐색
+        int zeroIdx = 0;
+        for(int i=0; i<remainStr.length(); i++) {
+            if(remainStr.charAt(i) == '0') {
+                zeroIdx = i+1;
             }
         }
         
-        if(ooz.length()>0){
-            //0이 없으면
-            if(sb.indexOf("0")==-1){
-                sb.insert(0, ooz);
+        StringBuilder res = new StringBuilder();
+        for(int i=0; i<zeroIdx; i++) {
+            res.append(remainStr.charAt(i));
+        }
+        for(int i=0; i<cnt110; i++) {
+            res.append("110");
+        }
+        for(int i=zeroIdx; i<remainStr.length(); i++) {
+            res.append(remainStr.charAt(i));
+        }
+        return res.toString();
+    }
+    
+    private static String extract110(String idx) {
+        Stack<Character> st = new Stack<>();
+        
+        for(int i=0; i<idx.length(); i++) {
+            if(st.size() >= 2 && idx.charAt(i) == '0') {
+                char mid = st.pop();
+                char first = st.pop();
+                if(first == '1' && mid == '1' && idx.charAt(i) == '0') continue;
+                else {
+                    st.push(first);
+                    st.push(mid);
+                    st.push(idx.charAt(i));
+                }
             }
-            else{
-                int idx = sb.lastIndexOf("0");
-                sb.insert(idx+1, ooz);
+            else {
+                st.push(idx.charAt(i));
             }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(char x : st) {
+            sb.append(x);
         }
         
         return sb.toString();
     }
+    
 }
