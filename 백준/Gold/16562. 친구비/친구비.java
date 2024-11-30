cc513f98a867e8c80e2e20420c2e4946248d1a11
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-11-30 20:04:18 +0900

    [Gold IV] Title: 친구비, Time: 164 ms, Memory: 18476 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -10,13 +10,10 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         /**
-         * 키워드
-         * 임의의 두 도시 사이에 길이 있을 수도, 없을 수도 있다.
-         * 가능한지 여부를 판별
-         * 같은 도시를 여러 번 방문하는 것도 가능
          * 접근법
-         * 유니온파인드가 트리 구조에서 노드의 최종 루트 부모를 판단하는것.
+         * 유니온파인드 문제
          * **/
+
         N = Integer.parseInt(st.nextToken());   //학생 수
         M = Integer.parseInt(st.nextToken());   //친구관계 수 (간선)
         K = Integer.parseInt(st.nextToken());   //가지고 있는 돈
@@ -30,39 +27,27 @@ class Main {
 
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            union(a,b);
+            union(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
         }
 
-        for(int i=1; i<=N; i++) find(i);
-
-        List<List<Integer>> list = new ArrayList<>();
-        for(int i=0; i<=N; i++) list.add(new ArrayList<>());
-
-        for(int i=1; i<=N; i++) list.get(parent[i]).add(i);
-
+        int[] price = new int[N+1];
+        Arrays.fill(price, Integer.MAX_VALUE);
         for(int i=1; i<=N; i++) {
-            if(list.get(i).isEmpty()) continue;
-            int min = Integer.MAX_VALUE;
-            for(int idx : list.get(i)) {
-                min = Math.min(min, cost[idx]);
-            }
-            if(min != Integer.MAX_VALUE) res += min;
+            price[find(i)] = Math.min(price[find(i)], cost[i]); //i의 부모
         }
+
+        for(int i=1; i<=N; i++) res += price[i] != Integer.MAX_VALUE ? price[i] : 0;
 
         System.out.println(res <= K ? res : "Oh no");
 
     }
-
     static void union(int a, int b) {
         int aRoot = find(a);
         int bRoot = find(b);
         parent[bRoot] = aRoot;
     }
-
     static int find(int x) {
         if(parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
+        return parent[x] = find(parent[x]); //좌표압축 필요없는 문제
     }
 }
