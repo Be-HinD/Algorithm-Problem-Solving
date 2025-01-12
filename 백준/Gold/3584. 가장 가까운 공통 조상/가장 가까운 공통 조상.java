4a5cfa78848da4f233e29fbdfa302d955f6ea739
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2025-01-12 19:13:19 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 172 ms, Memory: 22404 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
@@ -1,59 +1,45 @@
 import java.io.*;
 import java.util.*;
 
-//3584
+//BOJ_3584
 public class Main {
-    static int T, N, res;
-    static List<List<Integer>> list;
+    static int T, N;
+    static int[] node;
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        /**
-         * 접근법
-         * 두 노드의 겹치는 부모노드 탐색
-         * List로 트리 구성 + HashSet으로 공통 부모 탐색
-         * 검증
-         * 1. 두 노드의 공통 부모가 없을 경우 -> 트리 구조상 불가능
-         * 2. 한 노드가 나머지 노드의 부모가 되는 경우 -> 자기자신을 포함하여 Set추가
-         * **/
 
         T = Integer.parseInt(br.readLine());
         while(T-- > 0) {
             N = Integer.parseInt(br.readLine());
 
-            list = new ArrayList<>();
-            for(int i=0; i<=N; i++) list.add(new ArrayList<>());
+            node = new int[N+1];
+            boolean[] v = new boolean[N+1];
 
             for(int i=0; i<N-1; i++) {
                 st = new StringTokenizer(br.readLine());
 
                 int parent = Integer.parseInt(st.nextToken());
                 int child = Integer.parseInt(st.nextToken());
-                list.get(child).add(parent);
+                node[child] = parent;
             }
 
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            Set<Integer> aParent = new HashSet<>();
-            aParent.add(a);
-            while(!list.get(a).isEmpty()) {
-                a = list.get(a).get(0);
-                aParent.add(a);
-            }
-            
-            while(!list.get(b).isEmpty()) {
-                b = list.get(b).get(0);
-                if(aParent.contains(b)) {
-                    res = b;
-                    break;
-                }
-                aParent.add(b);
+            v[a] = true;
+            while(node[a] != 0) {
+                a = node[a];
+                v[a] = true;
             }
 
-            sb.append(res).append("\n");
+            while(!v[b]) {
+                b = node[b];
+            }
+
+            sb.append(b).append("\n");
         }
 
         System.out.println(sb);
