65ed326e555c1d75d2537c2c7d4462b6760847ee
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2025-01-31 23:08:26 +0900

    [Silver II] Title: 점프 점프, Time: 72 ms, Memory: 11852 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.java
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -10,36 +10,27 @@ public class Main {
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        dp = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        arr = new int[N];
         for(int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = Integer.MAX_VALUE;
         }
 
-        bfs();
-
-        System.out.println(dp[N-1] == Integer.MAX_VALUE ? -1 : dp[N-1]);
-
-    }
-
-    static void bfs() {
-        dp = new int[N];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{0,0});
         dp[0] = 0;
-
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-
-            for(int i=1; i<=arr[cur[0]]; i++) {
-                if(cur[0] + i >= N) continue;
-                if(dp[cur[0]+i] > cur[1]+1) {   //낮은 경우에만 탐색 진행
-                    q.offer(new int[]{cur[0]+i, cur[1]+1});
-                    dp[cur[0]+i] = cur[1] + 1;
+        for(int i=0; i<N; i++) {
+            if(dp[i] == Integer.MAX_VALUE) continue;
+            for(int j=i+1; j<=i+arr[i]; j++) {
+                if(j >= N) break;
+                if(dp[j] > dp[i] + 1) {
+                    dp[j] = dp[i] + 1;
                 }
             }
         }
+
+        System.out.println(dp[N-1] == Integer.MAX_VALUE ? -1 : dp[N-1]);
+
     }
 }
