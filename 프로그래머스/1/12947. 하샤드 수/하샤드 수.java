57911f04c8f19658d393a03623cdbe2a377007e9
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2025-02-02 16:44:33 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+        int temp = x;
+        int sum = 0;
+        while(temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        
+        return x % sum == 0 ? true : false;
+    }
+}
