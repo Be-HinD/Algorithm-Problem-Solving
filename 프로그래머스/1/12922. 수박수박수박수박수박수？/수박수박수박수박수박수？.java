c14f5abce6a4eaf6275be52c27765a860e63b7ed
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2025-02-05 02:17:13 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.00 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<n; i++) {
+            if((i&1) == 0) sb.append("수");
+            else sb.append("박");
+        }
+        return sb.toString();
+    }
+}
