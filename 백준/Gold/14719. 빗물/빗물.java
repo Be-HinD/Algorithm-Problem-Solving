ca8b1e1a504898ef87a60b59e76b78db9644dcb7
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2025-02-07 17:57:15 +0900

    [Gold V] Title: 빗물, Time: 68 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -11,25 +11,34 @@ public class Main {
         h = Integer.parseInt(st.nextToken());
         w = Integer.parseInt(st.nextToken());
 
+        /**
+         * i번째 열에서 채워져야 하는 빗물의 칸 수 == Math.min(왼쪽 최대값, 오른쪽 최대값)
+         * 전제조건 : 현재 칸 수 보다 왼쪽/오른쪽 최대값들이 커야함.
+         * 사용할 수 있는 스킬 : 현재 i원소의 왼쪽/오른쪽 최대값을 구하기 위한 누적합
+         * **/
         int[] arr = new int[w];
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<w; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        //왼쪽 최대값 배열
+        int[] leftMax = new int[w];
+        leftMax[0] = arr[0];
+        for(int i=1; i<w; i++) {
+            leftMax[i] = Math.max(arr[i], leftMax[i-1]);
+        }
+
+        //오른쪽 최대값 배열
+        int[] rightMax = new int[w];
+        rightMax[w-1] = arr[w-1];
+        for(int i=w-2; i>=0; i--) {
+            rightMax[i] = Math.max(arr[i], rightMax[i+1]);
+        }
+
         for(int i=1; i<arr.length-1; i++) {
-            // i를 기준으로 얼마나 채워져야 하는지 계산
-            int leftMax = arr[i];
-            for(int j=i-1; j>=0; j--) {
-                leftMax = Math.max(leftMax, arr[j]);
-            }
-
-            int rightMax = arr[i];
-            for(int j=i+1; j<arr.length; j++) {
-                rightMax = Math.max(rightMax, arr[j]);
-            }
-
-            res += Math.min(leftMax, rightMax) - arr[i];
+            if(leftMax[i-1] < arr[i] || rightMax[i+1] < arr[i]) continue;
+            res += Math.min(leftMax[i-1], rightMax[i+1]) - arr[i];
         }
 
         System.out.println(res);
