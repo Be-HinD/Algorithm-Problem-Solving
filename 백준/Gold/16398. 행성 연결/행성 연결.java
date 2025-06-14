2b222168e83ffb59527ad05f55454d76686ccee1
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2025-06-14 18:13:08 +0900

    [Gold IV] Title: 행성 연결, Time: 1324 ms, Memory: 177964 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+//BOJ_16398
+public class Main {
+    static class Node {
+        int x;
+        int y;
+        int w;
+        public Node(int x, int y, int w) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+        }
+    }
+    static int n;
+    static long res;
+    static int[] parent;
+    static List<Node> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+
+        list = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                list.add(new Node(i,j,Integer.parseInt(st.nextToken())));
+            }
+        }
+
+        Collections.sort(list, (o1, o2) -> o1.w - o2.w);
+
+        parent = new int[n];
+        for(int i=0; i<n; i++) parent[i] = i;
+
+        for(Node cur : list) {
+            if(!union(cur.x, cur.y, cur.w)) continue;
+            res += cur.w;
+        }
+
+        System.out.println(res);
+
+    }
+
+    static int find(int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static boolean union(int a, int b, int w) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+
+        if(aRoot == bRoot) return false;
+        parent[bRoot] = aRoot;
+
+        return true;
+    }
+}
