e5d02b2f91b934deeec67855c6a7117bcd632adf
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2025-10-05 16:28:28 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 16.53 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,31 +1,31 @@
 class Solution {
     public int[] solution(int[] sequence, int k) {
         int[] res = new int[2];
+        
         /**
+        <정석적인 구간합 문제>
         1. 길이가 짧은 수열 > 시작 인덱스가 낮은.
-        방법 1. 투포인터 : right를 늘리면서 커지면 left를 당기는 방식
-        방법 2. 누적합
+        방법 1. 투포인터 : right를 늘리면서 커지면 left를 당기는 방식 -> 풀이완
         **/
         
         int l=0, r=0, sum=sequence[0], min=Integer.MAX_VALUE;
         
         while(l<sequence.length) {
             if(sum == k) {
-                if(r-l < min) {
+                if(r-l < min) { // 시작 인덱스 조건절
                     res[0] = l; res[1] = r;
                     min = r-l;
                 }
             }
-            else if(sum > k){
+            
+            if(sum > k){
                 sum -= sequence[l++];
-                continue;
             }
-            if(r+1 < sequence.length) {
+            else {
+                if(r+1 >= sequence.length) break;
                 sum += sequence[++r];
             }
-            else break;
         }
-        
         
         return res;
     }
